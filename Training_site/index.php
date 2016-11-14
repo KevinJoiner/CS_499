c351e0df014cb09e0fd2f4310e7ec768fad0e3a9
kjja223 <kjja223@g.uky.edu>
2016-11-13 20:34:17 -0500

    homepage fix
    doesn't tell you to log in if you're already logged in

--- Training_site/index.php
+++ Training_site/index.php
@@ -4,18 +4,16 @@
 <div id="home_page">
  <body>
      <div>
-         <h2>Welcome User please login or sign up!</h2>
-		 <br>
-		 <br>
-		
-
-
-
+		<?php
+			if (isset($_SESSION['user'])){
+				echo '<h2>Hey there! Click the links on the header to navigate the site.</h2>';
+			}
+			else{
+				echo '<h2>Welcome User please login or sign up!</h2>';
+			}
+			
+		?>
 </body>
-
-
-
-
 </div>
 </html>
 
